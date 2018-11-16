c5de3ea7f06d2f87184fd88ca82bf4ef529afb6e
ubuntu <vittorio.nardone@gmail.com>
2018-11-16 11:35:46 +0100

    First model implementation - ready to train!

--- model.py
+++ model.py
@@ -23,11 +23,34 @@ class EncoderCNN(nn.Module):
 
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        pass
-    
+        """Set the hyper-parameters and build the layers."""
+        super(DecoderRNN, self).__init__()
+        self.embed = nn.Embedding(vocab_size, embed_size)
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
+        self.linear = nn.Linear(hidden_size, vocab_size)
+        
     def forward(self, features, captions):
-        pass
+        """Decode image feature vectors and generates captions."""
+        embeddings = self.embed(captions[:,:-1])
+        embeddings = torch.cat((features.unsqueeze(1), embeddings), 1)
+        hiddens, _ = self.lstm(embeddings)
+        outputs = self.linear(hiddens)
 
+        return outputs        
+        
     def sample(self, inputs, states=None, max_len=20):
-        " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
-        pass
+        """Generate captions for given image(s) features."""
+        sampled_ids = []
+        if (len(inputs.shape) == 2):
+            inputs = inputs.unsqueeze(1)
+        for i in range(max_len):
+            hiddens, states = self.lstm(inputs, states)          # hiddens: (batch_size, 1, hidden_size)
+            outputs = self.linear(hiddens.squeeze(1))            # outputs:  (batch_size, vocab_size)
+            _, predicted = outputs.max(1)                        # predicted: (batch_size)
+            sampled_ids.append(predicted)
+            inputs = self.embed(predicted)                       # inputs: (batch_size, embed_size)
+            inputs = inputs.unsqueeze(1)                         # inputs: (batch_size, 1, embed_size)
+        sampled_ids = torch.stack(sampled_ids, 1).tolist()       # sampled_ids: (batch_size, max_seq_length)
+        if (inputs.shape[0] == 1):
+            sampled_ids = sampled_ids[0]
+        return sampled_ids
